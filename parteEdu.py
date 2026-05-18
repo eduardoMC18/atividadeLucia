f9bdd95004e3e2cf8b7bc838c3e37dee9e0a6901
Eduardo Martins Colmati <eduardo.mc2@puccampinas.edu.br>
2026-05-18 10:32:18 -0300

    criando cadastrar pedido

---
+++ parteEdu.py
@@ -0,0 +1,12 @@
+def cadastrar_pedido():
+    novo_pedido = {
+        'id_pedido': 0,
+        'nome_cliente': 0,
+        'endereco': 0,
+        'prioridade': 0,
+        'descricao_pedido': 0,
+        'status_pedido': 0,
+        'id_entregador': 0
+    }
+    for i in novo_pedido:
+        return i
