786b5eee5011a792b3fee5154b75128eda0086c1
Enzo Carleti Teixeira <enzo.ct@puccampinas.edu.br>
2026-05-18 11:02:18 -0300

    adicionando buscar pedidos def

--- parteEdu.py
+++ parteEdu.py
@@ -5,8 +5,19 @@ def cadastrar_pedido():
         'endereco': 0,
         'prioridade': 0,
         'descricao_pedido': 0,
-        'status_pedido': 0,
+        'status_pedido': 'Pendente',
         'id_entregador': 0
     }
-    for i in novo_pedido:
-        return i
+
+def buscar_pedidos():
+    repeat = 0
+    print("Digite o ID do pedido:\n")
+    while repeat == 0:
+        buscar = int(input(""))
+        if buscar != novo_pedido['id_pedido']:
+            print("Erro, o ID não existe, digite outro ID:\n")
+        else:
+            print(novo_pedido)
+            repeat = 1
+    
+buscar_pedidos()
